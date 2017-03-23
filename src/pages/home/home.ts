982a0f720705ffe1745b76c636dd1dcd9fa0bfbb
Mr. Le Bosniaque <alvin.m@codeur.online>
2017-03-23 20:00:42 +0100

    Traduction faite excepte pour les projets tirés de la bdd

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -29,8 +29,6 @@ export class HomePage
 		//---------------------------SLIDER----------------------------------
     	$( document ).ready(function() 
     	{
-
-			var article = $(".articlesProjet");
 	
 			$("#flecheDroite").click(function(){
 				if ($("#articleProjet6").css("opacity") == "1") 
@@ -124,6 +122,51 @@ export class HomePage
 					$('#lienContact a').css({"color": "#F9643C"});
 				}
 			}, true);
+
+			$('.translator').on('click', function() {
+
+				var src = $('#imgTranslate').attr("src");
+				
+				if(src == 'images/france.png') {
+					$('#imgTranslate').attr("src", 'images/anglais.png');
+					$('#lienAccueil a').text('Accueil');
+					$('#lienProjets a').text('Projets');
+					$('#lienParcour a').text('Parcours');
+					$('#beginner').text('apprenti développeur web');
+					$('#voirProjets').text('Voir mes projets');
+					$('#aPropos p').text("Mon nom est Alvin Mujkic et je suis actuellement en formation à ACS ( Access Code School ) à Chalon-sur-Saône. Avant d'intégrer celle-ci, mon parcours n'est pas exceptionnel. Je n'ai jamais été très scolaire, donc, à part le Brevet des Collèges, je n'ai aucun diplôme. J'ai depuis surout effectué de petites missions en intérim qui ne m'apportait pas beaucoup de satisfaction. J'ai alors décider de me lancer la programmation pour les opportunités que ce domaine offre, et aussi car programmer me plaît. Tout d'abord, j'ai codé en ligne sur des sites de e-learning et sur différents forums. Mais ce nétai pas suffisant et il fallait intégrer une formation. Après plusieurs entretiens, j'ai trouvé ma place au sein de ma formation actuelle, ACS. Cette formation prépare au métier de dévelopeur logiciel (développeur web) et délivre un titre professionnel de niveau 3 (équivalent Bac+2).");
+					$('#articleCompetences h6 strong').text('Compétences');
+					$('#labelEmail').text('Votre e-mail :');
+					$('#labelSujet').text('Sujet :');
+					$('#message').attr('placeholder', 'Ecrivez votre message ici....');
+					$('#submit').attr('value', 'Envoyer');
+					$('#articleInfo h5').text('Informations complémentaires');
+					$('#idNom').text('Nom :');
+					$('#idPrenom').text('Prénom :');
+					$('#idTel').text('Tél :');
+					$('#idAdresse').text('Adresse :');
+				}
+
+				if(src == 'images/anglais.png') {
+					$('#imgTranslate').attr("src", 'images/france.png');
+					$('#lienAccueil a').text('Home');
+					$('#lienProjets a').text('Work');
+					$('#lienParcour a').text('Course');
+					$('#beginner').text('web developper (beginner)');
+					$('#voirProjets').text('View my work');
+					$('#aPropos p').text("My name is Alvin Mujkic et I'm curently in ACS (Access Code School) in Chalon-Sur-Saône. Before getting in this school, my general course is not fantastic. I never really liked the traditional school, so except the Certificate of General Education, I don't have any diplomas. Generally, I had many jobs, but they all were on short periods and dind't get me any satisfaction. Then I decided to learn programming because of all the opportunities the Web offers us, and mostly because it interest me a lot. I first started to learn coding on the Internet thanks to some e-learning sites et forums. But that wasn't enough and I wanted to find a school where I would be able to learn more efficiently. After a few interviews, I finally found and was accepted in ACS school. This school prepare us to be a junior web developper and provides us a diploma that is equivalent to the tray 2 level of education.");
+					$('#articleCompetences h6 strong').text('My skills');
+					$('#labelEmail').text('Your e-mail :');
+					$('#labelSujet').text('Subject :');
+					$('#message').attr('placeholder', 'Write your message here....');
+					$('#submit').attr('value', 'Send');
+					$('#articleInfo h5').text('Further informations');
+					$('#idNom').text('Surname :');
+					$('#idPrenom').text('Name :');
+					$('#idTel').text('Tel :');
+					$('#idAdresse').text('Adress :');
+				}
+			});
 
 		});
 
